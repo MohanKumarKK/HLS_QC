f44f1ec6b232d4409f2ade8c895c56165c616dbf
SoniyaH <60251715+SoniyaH@users.noreply.github.com>
2020-02-21 11:15:19 +0530

    Add files via upload

--- addDetails.go
+++ addDetails.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
         "io/ioutil"
 	"net/http"
+	"regexp"
 )
 
 type Detail struct{
@@ -38,8 +39,21 @@ func addDetails(w http.ResponseWriter, r *http.Request){
 		}
 
 		if detail.ProbeId != 0 && detail.ProbeOwner != "" && detail.Url != "" && detail.Ts_Segment != "" && detail.ErrorStatus != 0 && detail.ErrorDescription != "" && detail.Timestamp != ""{
-			w.Write([]byte(`{"message":"Successfully added"}`))
-			fmt.Println(detail)
+
+
+			matched, err:= regexp.MatchString(`[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]`,detail.Timestamp)
+			if(err != nil){
+				fmt.Println("Incorrect regular expression")
+			}
+			if(detail.ErrorStatus>=100 && detail.ErrorStatus<=599 && matched == true){
+				w.Write([]byte(`{"message":"Successfully added"}`))
+				fmt.Println(detail)
+			}else{
+				 w.WriteHeader(http.StatusBadRequest)
+	                         w.Write([]byte(`{"message":"Unsuccessful-Incorrect feild data"}`))
+
+			}
+
 		}else{
 			w.WriteHeader(http.StatusBadRequest)
 	                w.Write([]byte(`{"message":"Unsuccessful-Fields missing"}`))
